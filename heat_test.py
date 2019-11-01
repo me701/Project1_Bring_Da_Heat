b3e1965be63160478b74e349a1d565198c10faeb
Brandoyutz <Brandoyutz@gmail.com>
2019-11-01 17:02:18 -0500

    test file works?

--- heat_test.py
+++ heat_test.py
@@ -6,29 +6,35 @@ Created on Mon Oct 28 10:28:53 2019
 @author: brandon
 """
 
-import scipy as sp
+import sympy as sy
 import numpy as np
-import Heat1
+from Heat1 import Heat_1D
 import unittest
 
 class heat_test(unittest.TestCase):
     
     def Bn(self):
-        c = 1
         L = np.pi()
-        t = 0
+        x = L
         T1 = 100
         T2 = 0
-        n = 1  "What is n value?"
-        bfound = b_n(x, L, T1, T2, n, fun = lambda x: 100 ) 
+        n = 1
+        bfound = Heat_1D.b_n(x, L, T1, T2, n, fun = lambda x: 100 ) 
         bgiven = 200 / (n * np.pi()) * (1 - np.cos(n * np.pi()))
-        self.assertAlmostEqual(bfound, bgiven, places=4)
+        return self.assertAlmostEqual(bfound, bgiven, places=4)
         
-    def 1DHeat(self):
+    def DHeat(self):
         c = 1
         L = np.pi()
         t = 0
         T1 = 100
         T2 = 0
+        x = L
+        k = sy.symbol('k')
         hfound = Heat_1D(c, L, t, T1, T2, fun = lambda x: 100)
-#        hgiven = 400 / np.pi() sum np.exp(-(2 * k + 1)**2 * t / ((2 * k) + t )) * np.sin((2 * k) + 1) * x
+        expr = np.exp(-(2 * k + 1)**2 * t / ((2 * k) + t )) * np.sin((2 * k) + 1) * x
+        hgiven = 400 / np.pi() * sy.limit(expr, k, sy.oo)
+        return self.assertAlmostEqual(hfound, hgiven, places=4)
+        
+if __name__ == '__main__':
+    unittest.main()
