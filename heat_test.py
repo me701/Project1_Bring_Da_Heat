dd05e6ed9b9b428951e06b896ff7698b560044c3
Brandoyutz <Brandoyutz@gmail.com>
2019-11-02 15:17:15 -0500

    updated test

--- heat_test.py
+++ heat_test.py
@@ -6,20 +6,20 @@ Created on Mon Oct 28 10:28:53 2019
 @author: brandon
 """
 
-import sympy as sy
+import scipy as sp
 import numpy as np
-from Heat1 import Heat_1D
+import Heat1
 import unittest
 
 class heat_test(unittest.TestCase):
     
     def Bn(self):
         L = np.pi()
-        x = L
         T1 = 100
         T2 = 0
         n = 1
-        bfound = Heat_1D.b_n(x, L, T1, T2, n, fun = lambda x: 100 ) 
+        fun = lambda x: 100
+        bfound = sp.integrate.quad(Heat1.b_n,0, L, args=(L,T1,T2,n,fun,) )[0]
         bgiven = 200 / (n * np.pi()) * (1 - np.cos(n * np.pi()))
         return self.assertAlmostEqual(bfound, bgiven, places=4)
         
@@ -30,10 +30,10 @@ class heat_test(unittest.TestCase):
         T1 = 100
         T2 = 0
         x = L
-        k = sy.symbol('k')
-        hfound = Heat_1D(c, L, t, T1, T2, fun = lambda x: 100)
+        k = 1
+        hfound = Heat1.Heat_1D(c, L, t, T1, T2, fun = lambda x: 100)
         expr = np.exp(-(2 * k + 1)**2 * t / ((2 * k) + t )) * np.sin((2 * k) + 1) * x
-        hgiven = 400 / np.pi() * sy.limit(expr, k, sy.oo)
+        hgiven = 400 / np.pi() * Heat1.sumr(expr)
         return self.assertAlmostEqual(hfound, hgiven, places=4)
         
 if __name__ == '__main__':
