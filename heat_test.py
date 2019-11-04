6588db0081d84fe50f71a02d4638cc43ea98c64e
Brandoyutz <Brandoyutz@gmail.com>
2019-11-04 16:38:57 -0600

    Tests arent running

--- heat_test.py
+++ heat_test.py
@@ -8,33 +8,33 @@ Created on Mon Oct 28 10:28:53 2019
 
 import scipy as sp
 import numpy as np
-import Heat1
+from Heat1 import b_n, sumr, Heat_1D
 import unittest
 
 class heat_test(unittest.TestCase):
     
     def Bn(self):
-        L = np.pi()
+        L = np.pi
         T1 = 100
         T2 = 0
         n = 1
         fun = lambda x: 100
-        bfound = sp.integrate.quad(Heat1.b_n,0, L, args=(L,T1,T2,n,fun,) )[0]
-        bgiven = 200 / (n * np.pi()) * (1 - np.cos(n * np.pi()))
-        return self.assertAlmostEqual(bfound, bgiven, places=4)
+        bfound = sp.integrate.quad(b_n,0, L, args=(L,T1,T2,n,fun))[0]
+        bgiven = 200 / (n * np.pi * (1 - np.cos(n * np.pi)))
+        self.assertAlmostEqual(bfound, bgiven, places=4)
         
     def DHeat(self):
         c = 1
-        L = np.pi()
+        L = np.pi
         t = 0
         T1 = 100
         T2 = 0
         x = L
-        k = 1
-        hfound = Heat1.Heat_1D(c, L, t, T1, T2, fun = lambda x: 100)
+        k = 3
+        hfound = Heat_1D(c, L, t, T1, T2, fun = lambda x: 100)
         expr = np.exp(-(2 * k + 1)**2 * t / ((2 * k) + t )) * np.sin((2 * k) + 1) * x
-        hgiven = 400 / np.pi() * Heat1.sumr(expr)
-        return self.assertAlmostEqual(hfound, hgiven, places=4)
+        hgiven = 400 / np.pi * sumr(expr)
+        self.assertAlmostEqual(hfound, hgiven, places=4)
         
 if __name__ == '__main__':
     unittest.main()
