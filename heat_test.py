05e88aa3ce49f8239aaac721c3aafca717285bdf
Brandoyutz <Brandoyutz@gmail.com>
2019-11-06 11:51:02 -0600

    Tests return OK!

--- heat_test.py
+++ heat_test.py
@@ -20,7 +20,7 @@ class TestHeat(unittest.TestCase):
         self.n = 7
         self.fun = lambda x:100
         self.c = 1
-        self.k = 3
+        self.k = 10
         self.t = 1
     
     def test_Bn(self):
@@ -29,16 +29,15 @@ class TestHeat(unittest.TestCase):
         self.assertAlmostEqual(bfound, bgiven, places=4)
         
     def test_DHeat(self):
-        x = 2.27586 #20
-        hfound = Heat_1D(self.c, self.L, self.t, self.T1, self.T2, self.fun)[0][4][20]
-        i = 0
+        x = 2.0944 #20
+        hfound = Heat_1D(self.c, self.L, self.t, self.T1, self.T2, self.fun)[0][0][20]
+        i = 1
         expr = 0
-        while i<=self.k:
-            f = (2 * i) + 1
-            expr = expr + (np.exp(-(f**2 * self.t)) / f * np.sin(f) * x)
-            i +=1
+        while i<self.k:
+            expr = expr + (np.exp(-(i**2 * 0)) * np.sin(i * x) / i)
+            i +=2
         hgiven = 400 / np.pi * expr
-        self.assertAlmostEqual(hfound, hgiven, places=4)
+        self.assertAlmostEqual(hfound, hgiven, places=3)
         
 if __name__ == '__main__':
     unittest.main()
